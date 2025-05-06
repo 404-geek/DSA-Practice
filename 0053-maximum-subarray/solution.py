14d4b71a9f0c67452afcfae859e79a9bbd8d4e7f
Pranay Das <pranayd61@gmail.com>
2025-05-06 14:42:41 +0000

    Sync LeetCode submission Runtime - 62 ms (71.59%), Memory - 21.9 MB (23.48%)

--- 0053-maximum-subarray/solution.py
+++ 0053-maximum-subarray/solution.py
@@ -1,15 +1,21 @@
-class Solution:
-    def maxSubArray(self, nums: List[int]) -> int:
+class Solution(object):
+    def maxSubArray(self, nums):
+        """
+        :type nums: List[int]
+        :rtype: int
+        """
 
         curr_sum = 0
         max_sum = nums[0]
 
-        for n in nums:
+        for i in nums:
+
             if curr_sum < 0:
                 curr_sum = 0
+            
+            curr_sum += i
 
-            curr_sum += n
-
-            max_sum = max(max_sum, curr_sum)
-
-        return max_sum        
+            max_sum = max(curr_sum, max_sum)
+            
+        
+        return max_sum
