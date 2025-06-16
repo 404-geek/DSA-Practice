8280c55444ffc6de1a63030de7d3b333fcb596e9
Pranay Das <pranayd61@gmail.com>
2025-06-16 17:12:24 +0000

    Sync LeetCode submission Runtime - 150 ms (22.12%), Memory - 78.4 MB (30.43%)

--- 0146-lru-cache/solution.py
+++ 0146-lru-cache/solution.py
@@ -1,22 +1,38 @@
-class LRUCache:
+from collections import OrderedDict
+class LRUCache(object):
 
-    def __init__(self, capacity: int):
+    def __init__(self, capacity):
+        """
+        :type capacity: int
+        """
+        self.store = OrderedDict()
         self.capacity = capacity
-        self.cache = OrderedDict()
-
-    def get(self, key: int) -> int:
-        if key not in self.cache:
-            return -1
-        self.cache.move_to_end(key)
-        return self.cache[key]
-
-    def put(self, key: int, value: int) -> None:
-        if key in self.cache:
-            self.cache.move_to_end(key)
-        self.cache[key] = value
-        if len(self.cache) > self.capacity:
-            self.cache.popitem(last=False)
         
+
+    def get(self, key):
+        """
+        :type key: int
+        :rtype: int
+        """
+
+        if key in self.store:
+            self.store.move_to_end(key)
+            return self.store[key]
+        return -1
+
+        
+
+    def put(self, key, value):
+        """
+        :type key: int
+        :type value: int
+        :rtype: None
+        """
+        if key in self.store:
+            self.store.move_to_end(key)
+        self.store[key] = value
+        if len(self.store) > self.capacity:
+            self.store.popitem(last=False) 
 
 
 # Your LRUCache object will be instantiated and called as such:
