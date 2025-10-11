8a55744cb2545438c28d2d1aca924e781e825b6e
Pranay Das <pranayd61@gmail.com>
2025-10-11 01:03:31 +0000

    [LeetCode Sync] Runtime - 27 ms (56.01%), Memory - 18.8 MB (100.00%)

---
+++ leetcode/0030-substring-with-concatenation-of-all-words/solution.py
@@ -0,0 +1,45 @@
+class Solution:
+    def findSubstring(self, s: str, words: List[str]) -> List[int]:
+
+        words_cnt = Counter(words)
+        stats = defaultdict(int)
+        
+        k = len(words[0]) 
+        total = k * len(words)
+
+        res = []
+
+        for start in range(k):
+
+            left = right = start
+            word_counter = Counter()
+
+            while right + k <= len(s):
+
+                word = s[right:right + k]
+                right+=k
+
+                if word not in words_cnt:
+                    word_counter.clear()
+                    left = right
+                    continue
+                
+                word_counter[word]+=1
+
+                while word_counter[word] > words_cnt[word]:
+                    removed_word = s[left:left+k]
+
+                    left+=k
+                    word_counter[removed_word]-=1
+
+                if right - left == total:
+                    res.append(left)
+
+        return res
+
+            
+
+
+
+
+
