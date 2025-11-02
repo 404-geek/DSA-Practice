6bd56646438121f0abf01699219b1a481c533a7a
Pranay Das <pranayd61@gmail.com>
2025-11-02 19:50:11 +0000

    [LeetCode Sync] Runtime - 0 ms (100.00%), Memory - 19.9 MB (100.00%)

---
+++ leetcode/0098-validate-binary-search-tree/solution.py
@@ -0,0 +1,22 @@
+# Definition for a binary tree node.
+# class TreeNode:
+#     def __init__(self, val=0, left=None, right=None):
+#         self.val = val
+#         self.left = left
+#         self.right = right
+class Solution:
+    def isValidBST(self, root: Optional[TreeNode]) -> bool:
+
+        def dfs(node, min_, max_):
+            # base case
+            if node is None:
+                return True
+
+            if node.val <= min_ or node.val >= max_:
+                return False
+
+            return dfs(node.left, min_, node.val) and dfs(node.right, node.val, max_)
+
+        return dfs(root, float('-inf'), float('inf'))
+
+
