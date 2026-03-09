645d764a1ec35c447edc1d8c7537e48b8148a975
Pranay Das <pranayd61@gmail.com>
2026-03-09 19:57:03 +0000

    [LeetCode Sync] Runtime - 278 ms (13.40%), Memory - 31.4 MB (5.22%)

--- leetcode/0200-number-of-islands/solution.py
+++ leetcode/0200-number-of-islands/solution.py
@@ -1,34 +1,37 @@
 class Solution:
     def numIslands(self, grid: List[List[str]]) -> int:
-        rows = len(grid)
+
         cols = len(grid[0])
-        result = 0
-        visited = set()
+        rows = len(grid)
+        islands = 0
+        vis = set()
 
-        def dfs(r,c):
-            if (r,c) in visited:
+        def dfs(i,j):
+
+            if (i,j) in vis:
                 return
-            
-            visited.add((r,c))
 
-            exploring_nodes = [(0,1), (-1,0), (1,0),(0,-1)]
+            vis.add((i,j))
 
-            for dr, dc in exploring_nodes:
-                nc = c+dc
-                nr = r+dr
+            move = [(0,1), (1,0), (-1,0), (0,-1)]
 
-                if (0 <= nr < rows and 0 <= nc < cols and 
-                    grid[nr][nc] == "1" and (nr, nc) not in visited):
-                    dfs(nr, nc)
-            
+            for r,c in move:
+
+                nc = j + c
+                nr = i + r
+
+                if  0 <= nc < cols and 0 <= nr < rows and grid[nr][nc] == "1" and (nr,nc) not in vis:
+
+                    dfs(nr,nc)
+
         for i in range(rows):
             for j in range(cols):
-                if (i,j) not in visited and grid[i][j] == "1":
+                if (i,j) not in vis and grid[i][j] == "1":
+
                     dfs(i,j)
-                    result+=1
+                    islands+=1
 
+        return islands
 
-        return result                    
-        
-        
+            
         
