949b6fd13af8289526d2b4e398a57bf918c66ab3
Pranay Das <pranayd61@gmail.com>
2026-04-08 20:46:31 +0000

    [LeetCode Sync] Runtime - 0 ms (100.00%), Memory - 19.2 MB (78.24%)

---
+++ leetcode/0017-letter-combinations-of-a-phone-number/solution.py
@@ -0,0 +1,29 @@
+class Solution:
+    def letterCombinations(self, digits: str) -> List[str]:
+
+        digit_to_letters = {
+            "2": ["a", "b", "c"],
+            "3": ["d", "e", "f"],
+            "4": ["g", "h", "i"],
+            "5": ["j", "k", "l"],
+            "6": ["m", "n", "o"],
+            "7": ["p", "q", "r", "s"],
+            "8": ["t", "u", "v"],
+            "9": ["w", "x", "y", "z"]
+        }
+        
+
+        res = []
+        def backtrack(i , path):
+
+            if i == len(digits):
+                print(path)
+                res.append(path)
+                return
+
+            for ch in digit_to_letters[digits[i]]:
+                backtrack(i+1, path + ch)
+
+        backtrack(0, "")
+        
+        return res
