9b37e396877ee5077d23a24bde9f0694c3ddb92b
Pranay Das <pranayd61@gmail.com>
2026-04-09 02:34:52 +0000

    [LeetCode Sync] Runtime - 351 ms (68.44%), Memory - 44.4 MB (57.78%)

--- leetcode/1250-longest-common-subsequence/solution.py
+++ leetcode/1250-longest-common-subsequence/solution.py
@@ -1,14 +1,21 @@
 class Solution:
     def longestCommonSubsequence(self, text1: str, text2: str) -> int:
 
-        m, n = len(text1), len(text2)
-        dp = [[0] * (n + 1) for _ in range(m + 1)]
+        n = len(text1)
+        m = len(text2)
 
-        for i in range(m):
-            for j in range(n):
+        dp = [[0] * (m+1) for _ in range(n+1)]
+
+        for i in range(n):
+            for j in range(m):
+
                 if text1[i] == text2[j]:
-                    dp[i + 1][j + 1] = dp[i][j] + 1
+                    dp[i+1][j+1] = dp[i][j] + 1
                 else:
-                    dp[i + 1][j + 1] = max(dp[i][j + 1], dp[i + 1][j])
+                    dp[i+1][j+1] = max (dp[i][j+1] , dp[i+1][j])
 
-        return dp[m][n]
+                
+        return dp[n][m]
+
+
+        
