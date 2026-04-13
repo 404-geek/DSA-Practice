4a0a8fd875d6a17cb231a2c8c5d7db253ce3ffd3
Pranay Das <pranayd61@gmail.com>
2026-04-13 01:17:22 +0000

    [LeetCode Sync] Runtime - 627 ms (62.66%), Memory - 22.3 MB (54.10%)

--- leetcode/0015-3sum/solution.py
+++ leetcode/0015-3sum/solution.py
@@ -1,36 +1,50 @@
 class Solution:
     def threeSum(self, nums: list[int]) -> list[list[int]]:
-        
+
         nums = sorted(nums)
+
         n = len(nums)
+
         res = []
 
+
         for i in range(n - 2):
-            if i > 0 and nums[i] == nums[i - 1]:
+
+            if i > 0 and nums[i] == nums[i-1]:
                 continue
 
-            l = i+1
+            l = i+ 1
             r = n - 1
 
             while l < r:
 
-                to = nums[i] + nums[l] + nums[r]
+                su = nums[i] + nums[l] + nums[r]
 
-                if to < 0:
+                if su < 0:
                     l+=1
-                elif to > 0:
+                elif su > 0:
                     r-=1
+                              
                 else:
+
                     res.append([nums[i] , nums[l] , nums[r]])
+
                     l+=1
                     r-=1
 
-                    while  l < r  and nums[l-1] == nums[l]:
+                    while nums[l] == nums[l - 1] and l < r:
                         l+=1
-                    while l < r and nums[r+1] == nums[r]:
+                    
+                    while nums[r] == nums[r + 1] and l < r:
                         r-=1
-        
+
+
         return res
+
+
+
+
+
                 
 
 
