7c938aac35edf65fe5ffaed3f8e305a1ee2f524c
Pranay Das <pranayd61@gmail.com>
2026-04-17 04:47:39 +0000

    [LeetCode Sync] Runtime - 91 ms (49.49%), Memory - 31.1 MB (32.89%)

--- leetcode/0215-kth-largest-element-in-an-array/solution.py
+++ leetcode/0215-kth-largest-element-in-an-array/solution.py
@@ -1,15 +1,14 @@
 class Solution:
     def findKthLargest(self, nums: List[int], k: int) -> int:
 
-        min_heap = nums[:k]
-        heapq.heapify(min_heap)
-        
-        for num in nums[k:]:
-            if num > min_heap[0]:
-                heapq.heappushpop(min_heap, num)
-        
-        return min_heap[0]
+        heap = []
 
+        for n in nums:
 
-            
+            heapq.heappush(heap, n)
+
+            if len(heap) > k:
+                heapq.heappop(heap)
+        
+        return heap[0]
         
