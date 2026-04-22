0a24b0411b346bac7017f720044ec8fdf892850c
Pranay Das <pranayd61@gmail.com>
2026-04-22 21:28:02 +0000

    [LeetCode Sync] Runtime - 3 ms (60.72%), Memory - 20.2 MB (54.66%)

---
+++ leetcode/0662-maximum-width-of-binary-tree/solution.py
@@ -0,0 +1,40 @@
+# Definition for a binary tree node.
+# class TreeNode:
+#     def __init__(self, val=0, left=None, right=None):
+#         self.val = val
+#         self.left = left
+#         self.right = right
+class Solution:
+    def widthOfBinaryTree(self, root: Optional[TreeNode]) -> int:
+
+        if not root:
+            return None
+        
+        wid = 1
+        q = deque([(root, 0)])
+
+        while q:
+
+            level_len = len(q)
+            _ , first_index = q[0]
+
+            for _ in range(level_len):
+                node, index = q.popleft()
+                index-=first_index
+
+                if node.left:
+                    q.append((node.left, 2* index))
+                
+                if node.right:
+                    q.append((node.right , 2* index + 1))
+
+            wid = max(wid, index+1)
+
+        return wid
+
+                
+                    
+
+
+
+        
