6ceca4523087aae97c5bf4b7de98a7cf1389694c
Pranay Das <pranayd61@gmail.com>
2026-05-01 01:30:28 +0000

    [LeetCode Sync] Runtime - 15 ms (40.52%), Memory - 32.4 MB (21.34%)

---
+++ leetcode/0217-contains-duplicate/solution.py
@@ -0,0 +1,12 @@
+class Solution:
+    def containsDuplicate(self, nums: List[int]) -> bool:
+
+        m = set()
+
+        for i in nums:
+            if i in m:
+                return True
+            m.add(i)
+
+        return False
+        
