bd074972e50aabc0f7f9c96bbd2a779633f4168f
Pranay Das <pranayd61@gmail.com>
2026-05-01 01:24:46 +0000

    [LeetCode Sync] Runtime - 31 ms (81.88%), Memory - 20 MB (17.14%)

--- leetcode/0438-find-all-anagrams-in-a-string/solution.py
+++ leetcode/0438-find-all-anagrams-in-a-string/solution.py
@@ -1,40 +1,40 @@
 class Solution:
     def findAnagrams(self, s: str, p: str) -> List[int]:
 
-        p_count = Counter(p)
-        p_len = len(p)
+        c_str = len(p)
+        p = Counter(p)
 
-        start = 0
-        res = []
-        state = defaultdict(int)
+        s_len = len(s)
+        run_map = defaultdict(int)
 
+        ans = []
         have = 0
 
-        for end in range(len(s)):
+        l = 0
 
-            state[s[end]]+=1
+        for r in range(s_len):
 
-            if s[end] in p_count and state[s[end]] == p_count[s[end]]:
+            ch = s[r]
 
-                have+=1
+            if ch in p:
+                run_map[ch]+=1
 
-            if end - start + 1 == p_len:
-
-                if have == len(p_count):
-                    res.append(start)
-                
-                if s[start] in p_count and state[s[start]] == p_count[s[start]]:
-                    have-=1
-                state[s[start]]-=1
-                start+=1
+                if run_map[ch] == p[ch]:
+                    have+=1
             
-        return res
+            while r -l + 1 > c_str:
+                if s[l] in p:
+                    c = s[l]
 
-                
+                    if run_map[c] == p[c]:
+                        have-=1
+                    run_map[c]-=1
+                l+=1
+            
+            if r-l+1 == c_str and have == len(p):
+                ans.append(l)
 
-
-
-
+        return ans        
 
 
 
