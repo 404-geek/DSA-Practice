f87c28b5ed8ddfc85d1b48bdb76a3e46c4d8a85c
Pranay Das <pranayd61@gmail.com>
2026-05-05 16:34:53 +0000

    [LeetCode Sync] Runtime - 3 ms (86.53%), Memory - 19.5 MB (42.26%)

--- leetcode/0040-combination-sum-ii/solution.py
+++ leetcode/0040-combination-sum-ii/solution.py
@@ -1,31 +1,29 @@
 class Solution:
     def combinationSum2(self, candidates: List[int], target: int) -> List[List[int]]:
 
-        res = []
-
         n = len(candidates)
-        
+
         candidates.sort()
+        res = []
+        
+        def traverse(i, su, arr):
 
-        def back(rem, target, path):
-
-            if target == 0:
-                res.append(path[:])
+            if su == 0:
+                res.append(arr[:])
                 return
 
-            if target < 0:
-                return
-
-            for i in range(len(rem)):
-
-                if i > 0 and rem[i] == rem[i - 1]:
+            for j in range(i, n):
+                if j > i and candidates[j] == candidates[j - 1]:
                     continue
+                
+                if candidates[j] > su:
+                    break
 
-                path.append(rem[i])
-                back(rem[i+1:], target - rem[i], path)
-                path.pop()
+                arr.append(candidates[j])
+                traverse(j+1, su - candidates[j], arr)
+                arr.pop()
 
-
-        back(candidates, target, [])
+        traverse(0, target, [])
 
         return res
+
