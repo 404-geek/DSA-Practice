1c1a701801e1104b81f94709c23197d98aac8c07
Pranay Das <pranayd61@gmail.com>
2026-05-07 16:27:43 +0000

    [LeetCode Sync] Runtime - 2 ms (19.90%), Memory - 19.4 MB (86.36%)

--- leetcode/0050-powx-n/solution.py
+++ leetcode/0050-powx-n/solution.py
@@ -1,22 +1,35 @@
 class Solution:
     def myPow(self, x: float, n: int) -> float:
-                
-                
-        if n == 0:
-            return 1.0
 
+        # def pow(x,n):
+
+        #     if n == 0:
+        #         return 1
+
+        #     half = pow (x, n // 2)
+
+        #     if n % 2 == 0:
+        #         return half * half
+            
+        #     else:
+        #         return x * half * half
+
+        
         if n < 0:
-            x = 1 / x
+            x = 1/ x
             n = -n
 
-        res = 1.0
+        ans = 1
 
         while n > 0:
-            if n%2 == 1:
-                res *= x
-            x *=x
-            n //=2
 
-        return res
+            if n % 2 == 1:
+                ans *= x
+
+            x *= x
+            n //= 2
+
+        return ans
+
 
         
