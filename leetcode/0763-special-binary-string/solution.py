678d61e399836f19d8771b737676facc9db815ad
Pranay Das <pranayd61@gmail.com>
2026-05-09 01:32:33 +0000

    [LeetCode Sync] Runtime - 0 ms (100.00%), Memory - 19.1 MB (87.53%)

---
+++ leetcode/0763-special-binary-string/solution.py
@@ -0,0 +1,35 @@
+class Solution:
+    def makeLargestSpecial(self, s: str) -> str:
+
+        if len(s) <= 2:
+            return s
+
+        parts = []
+
+        start = 0
+
+        balance = 0
+
+        for i in range(len(s)):
+
+            if s[i] == '1':
+                balance += 1
+                
+            if s[i] == '0':
+                balance -= 1
+
+            if balance == 0:
+                ins = self.makeLargestSpecial(s[start + 1:i])
+
+                parts.append("1" +  ins + "0")
+
+                start = i+1
+
+        parts.sort(reverse=True)
+
+        return "".join(parts)
+
+
+
+
+
