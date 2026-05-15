7f3432eb1c300183547841f99395a6884b572dbd
Pranay Das <pranayd61@gmail.com>
2026-05-15 19:20:22 +0000

    [LeetCode Sync] Runtime - 3 ms (82.68%), Memory - 20.4 MB (61.30%)

--- leetcode/0283-move-zeroes/solution.py
+++ leetcode/0283-move-zeroes/solution.py
@@ -3,22 +3,14 @@ class Solution:
         """
         Do not return anything, modify nums in-place instead.
         """
-
         i = 0
+        n = len(nums)
 
-        for j in range(len(nums)):
-
+        for j in range(n):
             if nums[j] != 0:
-                nums[j], nums[i] = nums[i], nums[j]
+                nums[i], nums[j] = nums[j], nums[i]
                 i+=1
 
-        return nums
 
 
 
-
-
-
-
-
-        
