22379c301349ca9b2b8e1ce20a2a173fac676210
Pranay Das <pranayd61@gmail.com>
2026-05-17 04:51:37 +0000

    [LeetCode Sync] Runtime - 36 ms (40.54%), Memory - 21.9 MB (37.04%)

--- leetcode/0560-subarray-sum-equals-k/solution.py
+++ leetcode/0560-subarray-sum-equals-k/solution.py
@@ -1,19 +1,26 @@
 class Solution:
     def subarraySum(self, nums: List[int], k: int) -> int:
 
+        cnt = 0
+        curr = 0
+
         map = {0:1}
-        curr_sum = 0
-        ans = 0
 
         for n in nums:
 
-            curr_sum+=n
+            curr+=n
+            
+            if curr - k in map:
+                cnt += map[curr - k]
 
-            ans += map.get(curr_sum - k, 0)
+            if curr in map:
+                map[curr]+=1
+            else:
+                map[curr] = 1
 
-            map[curr_sum] = map.get(curr_sum, 0) + 1
+        return cnt
 
-        return ans
+        
 
 
-
+        
