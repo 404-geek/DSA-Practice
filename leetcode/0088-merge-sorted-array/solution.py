f4ca38da606cf44dcc82714c228522f335f72076
Pranay Das <pranayd61@gmail.com>
2026-05-18 00:41:10 +0000

    [LeetCode Sync] Runtime - 0 ms (100.00%), Memory - 19.4 MB (38.92%)

--- leetcode/0088-merge-sorted-array/solution.py
+++ leetcode/0088-merge-sorted-array/solution.py
@@ -3,27 +3,26 @@ class Solution:
         """
         Do not return anything, modify nums1 in-place instead.
         """
+        i = m -1
+        j = n- 1
+        k = m + n - 1
 
-        i = m-1
-        j = n-1
-        fill = m+n -1
+        while i >=0 and j >= 0:
 
-        while i >= 0 and j >= 0:
-
-            if nums2[j] > nums1[i]:
-                nums1[fill] = nums2[j]
+            if nums1[i] <= nums2[j]:
+                nums1[k] = nums2[j]
                 j-=1
+            
             else:
-                nums1[fill] = nums1[i]
-                nums1[i] = nums2[j]
+                nums1[k] = nums1[i]
                 i-=1
 
-            fill-=1
+            k-=1
 
         while j >= 0:
-            nums1[fill] = nums2[j]
+            nums1[k] = nums2[j]
             j-=1
-            fill-=1
+            k-=1
+
 
         
-            
