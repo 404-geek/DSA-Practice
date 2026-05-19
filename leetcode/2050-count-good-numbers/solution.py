badbc319a823a38a6f94156fdb18c542a9b2796f
Pranay Das <pranayd61@gmail.com>
2026-05-19 01:12:55 +0000

    [LeetCode Sync] Runtime - 0 ms (100.00%), Memory - 19.2 MB (90.66%)

--- leetcode/2050-count-good-numbers/solution.py
+++ leetcode/2050-count-good-numbers/solution.py
@@ -3,8 +3,9 @@ class Solution:
 
         MOD = 10 ** 9 + 7
 
-        evens = (n+1) // 2
-        odd = n // 2
+        even_positions = (n + 1) // 2
+        odd_positions = n // 2
 
-        return (pow(5, evens, MOD) * pow(4, odd, MOD)) % MOD
+        return (pow(5, even_positions, MOD) * pow(4, odd_positions, MOD)) % MOD
+
         
