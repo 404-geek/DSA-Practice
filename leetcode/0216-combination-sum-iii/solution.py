aca81d5963e297c6c8b5c823e71b4bdc8c339dce
Pranay Das <pranayd61@gmail.com>
2026-05-19 23:29:35 +0000

    [LeetCode Sync] Runtime - 0 ms (100.00%), Memory - 19.4 MB (34.14%)

--- leetcode/0216-combination-sum-iii/solution.py
+++ leetcode/0216-combination-sum-iii/solution.py
@@ -1,25 +1,23 @@
 class Solution:
     def combinationSum3(self, k: int, n: int) -> List[List[int]]:
-        
+
         res = []
 
-        def back(i, target, path):
+        def find_sum(path, a, j, su):
 
-            if len(path) == k:
-                if target == 0:
+            if a == 0:
+                if su == n:
+                    print("here")
                     res.append(path[:])
                 return
 
-            for j in range(i,10):
-
-                path.append(j)
-                back(j+1, target - j, path)
+            for i in range(j, 10):
+                if su + i > n:
+                    break
+                path.append(i)
+                find_sum(path, a - 1, i+1, su+i)
                 path.pop()
 
-        back(1, n, [])
-
+        find_sum([], k,1, 0)
         return res
-
-                
-
 
