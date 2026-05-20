059a729c54d6a5eb2299cfda7235a1b6a51c6e17
Pranay Das <pranayd61@gmail.com>
2026-05-20 01:46:57 +0000

    [LeetCode Sync] Runtime - 3 ms (74.04%), Memory - 19.6 MB (10.26%)

--- leetcode/0139-word-break/solution.py
+++ leetcode/0139-word-break/solution.py
@@ -1,25 +1,26 @@
 class Solution:
     def wordBreak(self, s: str, wordDict: List[str]) -> bool:
 
+        max_len = 0
+
+        for w in wordDict:
+            max_len = max(len(w), max_len)
+        
         wordDict = set(wordDict)
         n = len(s)
 
         @cache
         def traverse(i):
-
             if i == n:
                 return True
 
-            for word in wordDict:
-                if s.startswith(word, i):
-                    if traverse(i + len(word)):
+            for j in range(i, min(n, i + max_len)):
+
+                part = s[i:j+1]
+                if part in wordDict:
+                    if traverse(j+1):
                         return True
 
             return False
 
-        return traverse(0) 
-            
-
-
-
-        
+        return traverse(0)
