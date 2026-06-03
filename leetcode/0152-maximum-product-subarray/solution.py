4690b349efe36a36384ac90140419089899fc3a2
Pranay Das <pranayd61@gmail.com>
2026-06-03 21:46:49 +0000

    [LeetCode Sync] Runtime - 5 ms (52.26%), Memory - 20 MB (20.71%)

--- leetcode/0152-maximum-product-subarray/solution.py
+++ leetcode/0152-maximum-product-subarray/solution.py
@@ -1,26 +1,31 @@
 class Solution:
     def maxProduct(self, nums: List[int]) -> int:
-        
-        pref = 1
-        suff = 1
+
+        pref_prd = 1
+        suff_prd = 1
+
         n = len(nums)
-        max_p = nums[0]
+        max_prd = nums[0]
 
         for i in range(n):
 
-            if pref == 0:
-                pref = 1
-            if suff == 0:
-                suff = 1
+            val_pref = nums[i]
+            val_suff = nums[n-i-1]
 
-            pref *= nums[i]
-            suff *= nums[n- i - 1]
+            pref_prd *= val_pref
+            suff_prd *= val_suff
+
+            max_prd = max(max_prd, pref_prd, suff_prd)
             
-            max_p = max(max_p, max(pref, suff))
+            if val_pref == 0:
+                pref_prd = 1
+            
+            if val_suff == 0:
+                suff_prd = 1
 
-        return max_p
+        return max_prd
 
-                
+            
+            
 
-
-
+        
