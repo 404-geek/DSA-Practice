e30c3f907c54e1483629523691feec9befb05344
Pranay Das <pranayd61@gmail.com>
2026-06-12 01:04:48 -0400

    Add: lru-cache - submission-1

---
+++ Data Structures & Algorithms/lru-cache/submission-1.py
@@ -0,0 +1,23 @@
+class LRUCache:
+
+    def __init__(self, capacity: int):
+
+        self.lru = OrderedDict()
+        self.capacity = capacity
+        
+    def get(self, key: int) -> int:
+        if key in self.lru:
+            self.lru.move_to_end(key)
+        
+        return self.lru.get(key, -1)
+
+    def put(self, key: int, value: int) -> None:
+
+        self.lru[key] = value
+        self.lru.move_to_end(key)
+
+        if len(self.lru) > self.capacity:
+            self.lru.popitem(last=False)
+        
+
+        
