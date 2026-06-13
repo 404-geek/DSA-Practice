b18fec6fcb1a2b442e0d7ec36dadb779b87c9c3d
Pranay Das <pranayd61@gmail.com>
2026-06-13 19:45:22 -0400

    Add: course-schedule - submission-3

---
+++ Data Structures & Algorithms/course-schedule/submission-3.py
@@ -0,0 +1,36 @@
+class Solution:
+    def canFinish(self, numCourses: int, prerequisites: List[List[int]]) -> bool:
+
+        adj = {i : [] for i in range(numCourses)}
+
+        for a,b in prerequisites:
+            adj[b].append(a)
+
+        print(adj)
+
+        vis = [0] * numCourses
+
+        def dfs(node):
+            
+            if vis[node] == 1:
+                return False
+
+            if vis[node] == 2:
+                return True
+
+            vis[node] = 1
+
+            for nei in adj[node]:
+                if not dfs(nei):
+                    return False
+
+            vis[node] = 2
+
+            return True
+
+        for course in range(numCourses):
+            if not dfs(course):
+                return False
+
+        return True
+        
