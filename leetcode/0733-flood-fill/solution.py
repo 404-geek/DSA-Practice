f33ee8ddb294bf478199f9481a79042c2ff6ca42
Pranay Das <pranayd61@gmail.com>
2026-06-20 02:09:06 +0000

    [LeetCode Sync] Runtime - 0 ms (100.00%), Memory - 19.2 MB (99.58%)

--- leetcode/0733-flood-fill/solution.py
+++ leetcode/0733-flood-fill/solution.py
@@ -1,35 +1,25 @@
 class Solution:
     def floodFill(self, image: List[List[int]], sr: int, sc: int, color: int) -> List[List[int]]:
 
-        stack = [(sr,sc)]
-        original_color = image[sr][sc]
+        stack = [(sr, sc)]
+        val = image[sr][sc]
+        rows = len(image)
+        cols = len(image[0])
 
-        if original_color ==color:
+        if val == color:
             return image
-            
-        m = len(image)
-        n = len(image[0])
 
         while stack:
-            r, c = stack.pop()
-            
-            if not (0 <= r < m and 0 <= c < n):
-                continue  # skip out-of-bounds
 
-            if image[r][c] != original_color:
-                continue  # not part of the region
+            r,c = stack.pop()
 
-            image[r][c] = color
+            if 0 <= r < rows and 0 <= c < cols and image[r][c] == val:
+                image[r][c] = color
 
-            # Push neighbors
-            stack.append((r+1, c))
-            stack.append((r-1, c))
-            stack.append((r, c+1))
-            stack.append((r, c-1))
+                stack.append((r+1, c))
+                stack.append((r, c+1))
+                stack.append((r-1,c))
+                stack.append((r,c-1))
 
         return image
 
-
-
-        
-        
