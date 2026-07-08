323ca85ed15635675edaa8345c09cfda23d426cb
Pranay Das <pranayd61@gmail.com>
2026-07-08 03:07:21 +0000

    [LeetCode Sync] Runtime - 0 ms (100.00%), Memory - 19.4 MB (66.35%)

--- leetcode/0153-find-minimum-in-rotated-sorted-array/solution.py
+++ leetcode/0153-find-minimum-in-rotated-sorted-array/solution.py
@@ -11,7 +11,7 @@ class Solution:
             if nums[mid] > nums[r]:
                 l = mid + 1
             
-            elif nums[mid] < nums[r]:
+            else:
                 r = mid
 
         print(r)
